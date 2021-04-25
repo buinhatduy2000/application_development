7e7983d7c0cf84bea32310ff795f123a87049975
Miao <duybngch18459@fpt.edu.vn>
2021-04-25 15:51:46 +0700

    Update MVC for Staff

--- controllers/staff.controller.js
+++ controllers/staff.controller.js
@@ -2,7 +2,97 @@ var db = require("../db");
 var shortid = require('shortid');
 
 module.exports = {
+    //Account=============================================================
+    viewAccount: function (req, res) {
+        res.render('staff/viewAccount', {
+            accounts: db.get('accounts').value()
+        });
+    },
+    deleteAccount: function(req, res){
+        var id = req.params.id;
+        db.get('account').remove({id : id}).write();
+        res.redirect('/staff/viewAccount');
+        console.log(id);
+    },
+
+    //Course Category======================================================
+    viewCourseCategory: function (req, res) {
+        var category = db.get('courseCategory').value();
+        res.render('staff/viewCourseCategory', {
+            categorys: category
+        });
+    },
+
+    getCreateCourseCategory: function (req, res) {
+        var account = db.get('accounts').filter({ role: 'trainer' }).value();
+        res.render('staff/createCourseCategory', {
+            trainers: account
+        })
+    },
+
+    postCreateCourseCategory: function (req, res) {
+        req.body.id = shortid.generate();
+        db.get('courseCategory').push(req.body).write();
+        res.redirect('viewCourseCategory');
+    },
+    deleteCourseCategory: function(req, res){
+        var id = req.params.id;
+        db.get('courseCategory').remove({id : id}).write();
+        res.redirect('/staff/viewCourseCategory');
+        console.log(id);
+    },
+
+    //Course================================================================
+
+    viewCourse: function (req, res) {
+        var course = db.get('Course').value();
+        res.render('staff/viewCourse', {
+            courses: course
+        });
+    },
+
+    getCreateCourse: function (req, res) {
+        var categorys = db.get('courseCategory').value();
+        res.render('staff/createCourse', {
+            categorys: categorys
+        });
+    },
+    postCreateCourse: function (req, res) {
+        req.body.id = shortid.generate();
+        db.get('Course').push(req.body).write();
+        res.redirect('viewCourse');
+    },
+    deleteCourse: function(req, res){
+        var id = req.params.id;
+        db.get('Course').remove({id : id}).write();
+        res.redirect('/staff/viewCourse');
+        console.log(id);
+    },
+
+    //Topic==================================================================
+    viewTopic: function (req, res) {
+        var topic = db.get('topic').value();
+        res.render('staff/viewTopic', {
+            topics: topic
+        });
+    },
+    createTopic: function (req, res) {
+        res.render('staff/createTopic');
+    },
+    postCreateTopic: function (req, res) {
+        req.body.id = shortid.generate();
+        db.get('topic').push(req.body).write();
+        res.redirect('viewTopic');
+    },
+    deleteTopic: function(req, res){
+        var id = req.params.id;
+        db.get('topic').remove({id : id}).write();
+        res.redirect('/staff/viewTopic');
+        console.log(id);
+    },
+
+    //Home Page================================================================
     index: function (req, res) {
         res.render('staff/index');
-    }, 
+    },
 };
--- routes/staff.route.js
+++ routes/staff.route.js
@@ -1,8 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/staff.controller');
-// var validate = require('../validate/user.validate')
+var validate = require('../validate/admin.validate')
 // var authMiddleware = require('../middlewares/auth.middleware')
+
+//Account
+router.get('/viewAccount', controller.viewAccount);
+
+router.get('/deleteAccount/:id', controller.deleteAccount);
+
+//Course Category
+router.get('/viewCourseCategory', controller.viewCourseCategory);
+
+router.get('/createCourseCategory', controller.getCreateCourseCategory);
+
+router.post('/createCourseCategory',validate.postCreateCourseCategory, controller.postCreateCourseCategory);
+
+router.get('/deleteCourseCategory/:id', controller.deleteCourseCategory);
+
+//Course
+router.get('/viewCourse', controller.viewCourse);
+
+router.get('/createCourse', controller.getCreateCourse);
+
+router.post('/createCourse',validate.postCreateCourse ,controller.postCreateCourse);
+
+router.get('/deleteCourse/:id', controller.deleteCourse);
+
+//Topic
+router.get('/viewTopic', controller.viewTopic);
+
+router.get('/createTopic', controller.createTopic);
+
+router.post('/createTopic', controller.postCreateTopic),
+
+router.get('/deleteTopic/:id', controller.deleteTopic);
 
 router.get('/', controller.index);
 
