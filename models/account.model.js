46590a4f791a4175853603196d76f589308d5832
nvluc-git <74579767+nvluc-git@users.noreply.github.com>
2021-05-04 17:45:25 +0700

    Update account.model.js

--- models/account.model.js
+++ models/account.model.js
@@ -6,7 +6,11 @@ const Account = new Schema({
     name: {type: String, maxLength: 255},
     username: {type: String, maxLength: 255},
     password: {type: String, maxLength: 255},
-    role: {type: String, maxLength: 255}
+    role: {type: String, maxLength: 255},
+    age: { type: String, maxLength: 255 },
+    dateofbirth: {type: Date, maxLength: 255},
+    education: {type: String, maxLength: 255},
+    mainprogramminglanguage: {type: String, maxLength: 255}
 });
 
 module.exports = mongoose.model('Account', Account);
