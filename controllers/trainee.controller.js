54fa3b80731f20ad4d80cbb8cc9d6a3127fb6d7e
thanhan7501 <anntgch190372@fpt.edu.vn>
2021-05-04 14:56:24 +0700

    fix trainee

--- controllers/trainee.controller.js
+++ controllers/trainee.controller.js
@@ -1,7 +1,32 @@
-var db = require("../db");
-var shortid = require('shortid');
+var Course = require("../models/course.model");
+var CourseCategory = require("../models/courseCategory.model")
+var Topic = require("../models/topic.model");
 
 module.exports = {
+
+    viewCourseCategory: async function (req, res) {
+        var category =  await CourseCategory.find({});
+        res.render('trainee/viewCourseCategory', {
+            categorys: category
+        });
+    },
+
+    viewCourse: async function (req, res) {
+        var category = req.params.category;
+        var course = await Course.find({courseCategory: category});
+        res.render('trainee/viewCourse', {
+            courses: course,
+            category: category
+        });
+    },
+
+    viewTopic: async function (req, res) {
+        var course = req.params.course
+        var topic = await Topic.find({courseName: course})
+        res.render('trainee/viewTopic', {
+            topics: topic
+        });
+    },
 
     //Home Page================================================================
     index: function (req, res) {
--- routes/trainee.route.js
+++ routes/trainee.route.js
@@ -2,6 +2,16 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/trainee.controller');
 
+
+//Course Category
+router.get('/viewCourseCategory', controller.viewCourseCategory);
+
+//Course
+router.get('/viewCourse/:category', controller.viewCourse);
+
+//Topic
+router.get('/viewTopic/:course', controller.viewTopic);
+
 router.get('/', controller.index);
 
 module.exports = router;
